587e5f2acfd24b0058f5113fe39482602da6e68d
Musab Gulfam <musabgulfam0722@gmail.com>
2021-08-12 01:18:45 +0500

    Added first basic component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@material-ui/core/styles'
+import Header from './ui/Header'
+import theme from './ui/Theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
